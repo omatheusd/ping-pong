389ed371f30c8b43021743b8b0aa5147dcd51a6e
omatheusd <matheus.defaveri21@gmail.com>
2025-01-28 19:05:55 -0300

    Add files via upload
    aula dia 28

--- bola.gd
+++ bola.gd
@@ -1,23 +1,23 @@
 extends Node2D
 
 var velocy: int = 100
+var velocx: int = 100
 var diry: int = 1
 var dirx: int = 1
-var velocx: int = 20
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y = position.y + diry *  (velocy * delta)
-	position.x = position.x + dirx *  (velocx * delta)
+	position.y = position.y + diry * (velocy * delta)
+	position.x = position.x + dirx * (velocx * delta)
+	#print(position)
 	if position.y >= 640:
 		diry = -diry
+		#position.y = 640
 	elif position.y <= 10:
-		diry = -diry
-	if position.x >= 100:
-		dirx = -dirx
+		diry = - diry
+	elif position.x >= 1142:
+		dirx = - dirx
 	elif position.x <= 10:
-		dirx = -dirx
+		dirx = - dirx
--- raquete.gd
+++ raquete.gd
@@ -1,6 +1,6 @@
-extends Sprite2D
-var velox: int = 100
+extends Node2D
 
+var speedx: int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +9,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("dire"):
-		position.x = position.x + velox + delta
+		position.x = position.x + speedx * delta
+	elif Input.is_action_pressed("esq"):
+		position.x = position.x - speedx * delta
+		
+	if position.x >= 1050:
+		position.x = 1050
+	elif position.x <= 80:
+		position.x = 80
+	print("raquete esta em: ", position, "posicao")
+	
