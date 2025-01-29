7f0409b9033176fa4ddf4c886742a5113e0c9745
omatheusd <matheus.defaveri21@gmail.com>
2025-01-28 22:15:31 -0300

    Add files via upload

---
+++ imagens/bola.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var velocy: int = 100
+var velocx: int = 100
+var diry: int = 1
+var dirx: int = 1
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	position.y = position.y + diry * (velocy * delta)
+	position.x = position.x + dirx * (velocx * delta)
+	#print(position)
+	if position.y >= 640:
+		diry = -diry
+		#position.y = 640
+	elif position.y <= 10:
+		diry = - diry
+	elif position.x >= 1142:
+		dirx = - dirx
+	elif position.x <= 10:
+		dirx = - dirx
+
+
+func acertaramBola(area: Area2D) -> void:
+	diry = -diry
---
+++ imagens/raquete.gd
@@ -0,0 +1,22 @@
+extends Node2D
+signal emit
+var speedx: int = 200
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("dire"):
+		position.x = position.x + speedx * delta
+	elif Input.is_action_pressed("esq"):
+		position.x = position.x - speedx * delta
+		
+	if position.x >= 1050:
+		position.x = 1050
+	elif position.x <= 80:
+		position.x = 95
+
+func acertaRaquete(area: Area2D) -> void:
+	pass
