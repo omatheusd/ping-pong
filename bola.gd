71601658f71df20738d77b1ca8578d77a5d6be9c
omatheusd <matheus.defaveri21@gmail.com>
2025-01-29 22:20:31 -0300

    Add files via upload

--- bola.gd
+++ bola.gd
@@ -1,23 +1,53 @@
 extends Node2D
 
-var velocy: int = 100
-var velocx: int = 100
+class_name BOLA
+
+var velocy: int = randi_range(300,500)
+var velocx: int = randi_range(50,400)
 var diry: int = 1
-var dirx: int = 1
+var dirx: int = 0
+
+signal gameover #indica o gameover para as outras classes
 
 func _ready() -> void:
-	pass # Replace with function body.
+	var aleatorio = randi_range(0,1)
+	if aleatorio == 1:
+		dirx = 1
+	else:
+		dirx = -1
+	print("Dirx vale ", dirx)
 
 func _process(delta: float) -> void:
 	position.y = position.y + diry * (velocy * delta)
 	position.x = position.x + dirx * (velocx * delta)
-	#print(position)
-	if position.y >= 640:
-		diry = -diry
+	print("bola esta em: ", position)
+	if position.x <= 0:
+		gameover.emit()
+		resetarPokemon()
+		#diry = -diry
 		#position.y = 640
-	elif position.y <= 10:
+	elif position.y <= 25:
 		diry = - diry
-	elif position.x >= 1142:
-		dirx = - dirx
-	elif position.x <= 10:
-		dirx = - dirx
+	elif position.y >= 610:
+		diry = - diry
+	if position.x >= 1145:
+		dirx = -dirx
+var estimativa = 0
+
+func resetarPokemon() -> void:
+	velocy = randi_range(300,500)
+	velocx = randi_range(50,400)
+	get_node("Pokemon1").visible = true
+	get_node("Pokemon2").visible = false
+	estimativa = 0
+
+func acertaramBola(area: Area2D) -> void:
+	#print(area.name)
+	dirx = -dirx
+	velocx += randi_range(1, 50)
+	velocy += randi_range(10, 100)
+	estimativa +=1
+	if estimativa > 4:
+		get_node("Pokemon1").visible = false
+		get_node("Pokemon2").visible = true
+	
--- raquete.gd
+++ raquete.gd
@@ -1,21 +1,37 @@
 extends Node2D
 
-var speedx: int = 200
+class_name RAQUETE
+
+var ponto: int = 0
+var speedx: int = 300
+
+signal acertou
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("dire"):
-		position.x = position.x + speedx * delta
-	elif Input.is_action_pressed("esq"):
-		position.x = position.x - speedx * delta
-		
-	if position.x >= 1050:
-		position.x = 1050
-	elif position.x <= 80:
-		position.x = 80
-	print("raquete esta em: ", position, "posicao")
+	if Input.is_action_pressed("baixo"):
+		position.y = position.y + speedx * delta
+	elif Input.is_action_pressed("cima"):
+		position.y = position.y - speedx * delta
 	
+	if position.y <= 100:
+		position.y = 100
+	elif position.y >= 541:
+		position.y = 541
+	#print("raquete esta em: ", position, "posicao")
+
+func retornaPonto () -> int:
+	return ponto
+
+func resetPonto () -> void:
+	ponto = 0
+
+
+func acertaramRaquete(area: Area2D) -> void:
+	ponto = ponto + 1
+	print("pontuacao vale ",ponto)
+	acertou.emit()
